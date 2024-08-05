f27f82b678802baf8b94cab1df31c5abab4d7135
Aleksandr Shamko <shamkoag@gmail.com>
2024-08-05 17:55:10 +0200

    feat:add new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+	const myArray = n.toString().split('');
+	const reversed = myArray.reverse();
+	const toString = reversed.join('');
+	const number = parseInt(toString);
+	const result = Math.abs(number);
+	return result;
 }
