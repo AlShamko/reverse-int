dd0fa68f501a9b32adc02d53b10bc52d5e59ee48
AlShamko <shamkoag@gmail.com>
2025-08-01 10:01:26 +0200

    added new function that reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const numStr = Math.abs(n).toString();
+  const reversedStr = numStr.split('').reverse().join('');
+  const result = parseInt(reversedStr, 10);
+  return result;
 };
